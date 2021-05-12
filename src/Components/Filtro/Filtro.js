78ace13f708d92d8fe74fc7955271208cc47d01a
hgalvao98 <henrique.galvao@hotmail.com>
2021-05-12 17:30:01 -0300

    estatica

--- src/Components/Filtro/Filtro.js
+++ src/Components/Filtro/Filtro.js
@@ -4,9 +4,9 @@ import styled from 'styled-components';
 const ContainerFiltro = styled.div`
 display:flex;
 flex-direction:column;
-height:40vh;
-width:20%;
-border-left:2px solid black;
+height:100vh;
+width:100%;
+border-right:2px solid black;
 h3{
     padding:8px;
 }`
@@ -15,9 +15,9 @@ const ContainerConteudo = styled.div`
 display:grid;
 justify-content:left;
 padding:4px;
-border:1px solid black;
+// border:1px solid black;
 `
-export default class filtro extends React.Component {
+export default class Filtro extends React.Component {
     render() {
         return (
             <ContainerFiltro>
---
+++ src/Components/Produto/ProdutoCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './App.css';
+import styled from 'styled-components'
+
+
+
+
+export default class ProdutoCard extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Teste</h1>
+            </div>
+        )}}
