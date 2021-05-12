fa2999705fe17870fc39be0fd5cf928df7319278
unknown <Administrador@CETEM-2580.mineral.cetem>
2021-05-12 15:07:40 -0300

    filtro basico

---
+++ src/components/Filtro.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ContainerFiltro = styled.div`
+display:flex;
+flex-direction:column;
+height:40vh;
+width:20%;
+border-left:2px solid black;
+h3{
+    padding:8px;
+}`
+
+const ContainerConteudo = styled.div`
+display:grid;
+justify-content:left;
+padding:4px;
+border:1px solid black;
+`
+export default class filtro extends React.Component {
+    render() {
+        return (
+            <ContainerFiltro>
+                <ContainerConteudo>
+                    <h3>Filtro:</h3>
+                    <p>Valor mínimo:</p>
+                    <input type="number"></input>
+                    <p>Valor Máximo:</p>
+                    <input type="number"></input>
+                    <p>Busca por nome:</p>
+                    <input type="text"></input>
+                </ContainerConteudo>
+
+            </ContainerFiltro>
+
+        );
+    }
+}
+
